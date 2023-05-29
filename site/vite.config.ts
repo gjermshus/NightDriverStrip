8e71ec2af15d8b8aaaad08e6eed5bef7f6f682e8
Anders Gjermshus <gjermshus@gmail.com>
2023-05-29 23:46:31 +0200

    Add production build using Vite

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,11 +1,26 @@
 // default vite config
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteExternalsPlugin } from 'vite-plugin-externals';
+
+const viteExternals = viteExternalsPlugin({
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM',
+    'react-router': 'ReactRouter',
+    'material-ui': 'MaterialUI',
+    '@material-ui': 'MaterialUI',
+    "recharts": "Recharts",
+    "@mui": "MaterialUI"
+})
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), viteExternals],
     server: {
         port: 3000,
     },
-    publicDir: './local'
+    publicDir: './local',
+    build: {
+        outDir: './dist',
+    },
 });
